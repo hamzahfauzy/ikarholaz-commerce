95a3a931c9d36ffac6b574d802091afa90f8aa23
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-02-13 11:30:50 +0700

    tes lagi

--- app/Models/WaBlast.php
+++ app/Models/WaBlast.php
@@ -78,4 +78,36 @@ class WaBlast
         sleep(1); #do not delete!
         return $response;
     }
+
+    static function sent($to, $message)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://hp.fonnte.com/api/send_message.php",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => array(
+                'phone' => $to,
+                'type' => 'text',
+                'text' => $message,
+                'delay' => '1',
+                'schedule' => '0'),
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: ".getenv('WA_FONNTE_DEVICE')
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+
+        curl_close($curl);
+        sleep(1); #do not delete!
+        return $response;
+    }
 }
