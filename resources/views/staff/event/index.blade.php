6875905bf0df736cd25656f8dee17ee7c1a1cdc2
fadillahrizki <fadillahrizki16@gmail.com>
2022-08-05 15:13:14 +0700

    add export events

--- resources/views/staff/event/index.blade.php
+++ resources/views/staff/event/index.blade.php
@@ -28,16 +28,19 @@
                             </span>
 
                             <div class="float-right">
-                                <a href="{{ route('staff.events.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
+                                <a href="{{ route('staff.events.create') }}" class="btn btn-primary btn-sm mr-3"  data-placement="left">
                                     {{ __('Create New') }}
                                 </a>
+                                <button class="btn btn-success btn-sm float-right btn-export">
+                                  {{ __('Export') }}
+                                </button>
                             </div>  
                         </div>
                     </div>
 
                     <div class="card-body">
                         <div class="table-responsive">
-                            <table class="table table-striped table-hover">
+                            <table class="table table-striped table-hover tbl-events">
                                 <thead class="thead">
                                     <tr>
                                         <th>No</th>
@@ -48,7 +51,7 @@
 										<th>Waktu Selesai</th>
 										<th>Tempat</th>
 
-                                        <th></th>
+                                        <th class="noExl"></th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -62,7 +65,7 @@
 											<td>{{ $event->end_time }}</td>
 											<td>{{ $event->place }}</td>
 
-                                            <td>
+                                            <td class="noExl">
                                                 <form action="{{ route('staff.events.destroy',$event->id) }}" method="POST" onsubmit="if(confirm('{{__('Are you sure to delete this item ?')}}')){ return true }else{ return false }">
                                                     <a class="btn btn-sm btn-primary " href="{{ route('staff.events.show',$event) }}"><i class="fa fa-fw fa-eye"></i> Show</a>
                                                     <a class="btn btn-sm btn-success" href="{{ route('staff.events.edit',$event) }}"><i class="fa fa-fw fa-edit"></i> Edit</a>
@@ -82,4 +85,20 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('script')
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="{{asset('js/jquery.table2excel.js')}}"></script>
+<script>
+$(".btn-export").click(function(){
+  $(".tbl-events").table2excel({
+    // exclude CSS class
+    exclude: ".noExl",
+    name: "Laporan Kegiatan",
+    filename: "LaporanKegiatan", //do not include extension
+    fileext: ".xls" // file extension
+  }); 
+});
+</script>
 @endsection
