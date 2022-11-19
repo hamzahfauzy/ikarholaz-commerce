5086c2d2579d4365bd398efdf0556a80ac811519
hamzahfauzy <hamzahfauzy97@gmail.com>
2022-11-19 10:58:17 +0700

    fix event not found

--- routes/api.php
+++ routes/api.php
@@ -51,7 +51,7 @@ Route::post('/get-alumnis', [BaseController::class, 'getAlumnis']);
 Route::prefix('events')->group(function(){
     Route::get('/',[EventController::class,'index']);
     Route::post('create',[EventController::class,'store']);
-    Route::post('update/{id}',[EventController::class,'update']);
+    Route::put('update/{event}',[EventController::class,'update']);
     Route::delete('delete/{id}',[EventController::class,'destroy']);
     Route::get('show/{id}',[EventController::class,'show']);
 });
